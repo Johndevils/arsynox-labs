35f6ff93614d16b6dbb05916ad93151fc8f8f886
Mr Lucifer <los7phantom@duck.com>
2026-01-04 02:32:12 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,18 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: [
     "./index.html",
-    "./src/**/*.{ts,tsx}"
+    "./src/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
+      },
+      boxShadow: {
+        glow: "0 0 35px rgba(99,102,241,0.35)",
       },
     },
   },
